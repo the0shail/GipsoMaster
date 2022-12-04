0c9a889c0ce0eed0f827c043875f4e047ef70b65
the0shail <nespasibo179@gmail.com>
2022-12-04 15:22:35 +0500

    Finish commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.testimonials-slider').owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        nav: true,
+        dots: false
+
+    });
+});
